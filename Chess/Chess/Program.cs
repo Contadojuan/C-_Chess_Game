753c8a04d618ec2239987488ba0ab394957b3378
Juan Paixao <juanpaixao@live.com>
2018-07-20 21:50:46 -0300

    Finishing the board positions
    Now i have an (8,8) array (my board) created

--- Chess/Chess/Program.cs
+++ Chess/Chess/Program.cs
@@ -12,8 +12,10 @@ namespace Chess
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
-
+            Tela.imprimirTabuleiro(tab);
             Console.ReadLine();
+
+         
         }
     }
 }
